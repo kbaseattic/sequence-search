26151fa0057fc6a11dc9cac3fea4664be1a51ef4
David Lyon <dauglyon@users.noreply.github.com>
2020-10-09 12:32:26 -0700

    Adds ResultContent component

--- react_app/src/components/SearchResults.tsx
+++ react_app/src/components/SearchResults.tsx
@@ -1,17 +1,41 @@
 import React, { FC } from 'react';
-import { Spin, List, Avatar, PageHeader, Card } from 'antd';
+import { Spin, List, Table, Avatar, PageHeader, Card, Tabs } from 'antd';
 import { Search } from '../types/Search';
 
 const ResultContent: FC<{ search: Search }> = ({ search }) => {
   if (search.status !== "completed") return <React.Fragment />;
   return (
-    <Card>
-      <Spin spinning={search.result === undefined}>
-        <pre>
-          {JSON.stringify(search.result, null, 2)}
-        </pre>
-      </Spin>
-    </Card>
+    <Spin spinning={search.result === undefined}>
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Table" key="1">
+          <Table
+            tableLayout="fixed"
+            dataSource={search.result?.alignments || []}
+            rowKey={record => JSON.stringify(record)}
+            columns={[
+              { title: "E-value", dataIndex: "evalue", sorter: true },
+              { title: "Bit-score", dataIndex: "bitscore", sorter: true },
+              { title: "Query", dataIndex: "queryid", sorter: true },
+              { title: "Target", dataIndex: "targetid", sorter: true },
+            ]}
+            expandable={{
+              expandedRowRender: record => (
+                <Card style={{ width: "100%" }}>
+                  <pre>
+                    {`Query  ${record.queryalignstart} ${record.queryalignseq}\nTarget ${record.targetalignstart} ${record.targetalignseq}`}
+                  </pre>
+                </Card>
+              ),
+            }}
+          />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Raw JSON" key="2">
+          <pre>
+            {JSON.stringify(search.result, null, 2)}
+          </pre>
+        </Tabs.TabPane>
+      </Tabs>
+    </Spin >
   );
 };
 
@@ -25,9 +49,11 @@ export const SearchResults: FC<{ searches: Search[]; }> = ({ searches }) => {
         renderItem={search => (
           <List.Item>
             <List.Item.Meta
-              avatar={<Spin spinning={search.status != "completed"}>
-                <Avatar>{search.ticketId}</Avatar>
-              </Spin>}
+              avatar={(
+                <Spin spinning={search.status != "completed"}>
+                  <Avatar>{search.ticketId}</Avatar>
+                </Spin>
+              )}
               title={`Search #${search.ticketId}`}
               description={`Status: ${search.status}`} />
             <ResultContent search={search} />
