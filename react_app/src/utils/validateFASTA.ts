ec412f1b6ba08fd65da49c84124d327d383ffb12
David Lyon <dauglyon@users.noreply.github.com>
2020-10-14 12:08:54 -0700

    Squash lint warnings and clean up fragile hooks

--- react_app/src/utils/validateFASTA.ts
+++ react_app/src/utils/validateFASTA.ts
@@ -2,10 +2,10 @@ export async function validateFASTA(fasta: string) {
   const lines = fasta.split("\n").map((l) => l.trimRight());
   const headers = lines
     .map((l, i) => (l.startsWith(">") && l.length > 1 ? i : -1))
-    .filter((hloc) => hloc != -1);
+    .filter((hloc) => hloc !== -1);
 
   // ensure 1 or more headers exist
-  if (headers.length === 0) throw "No FASTA headers found";
+  if (headers.length === 0) throw new Error("No FASTA headers found");
 
   // sequence groups
   const seqs = headers.map((hloc) => {
@@ -19,10 +19,10 @@ export async function validateFASTA(fasta: string) {
   // check that each sequence contains only allowed chars
   seqs.forEach(([start, stop]) => {
     const seq = lines.slice(start, stop);
-    if (seq.length < 1) throw `Empty sequence on L${start}`;
+    if (seq.length < 1) throw new Error(`Empty sequence on L${start}`);
     seq.forEach((l, i) => {
       if (!/^[ACDEFGHIKLMNPQRSTUVWY\s]+$/i.test(l))
-        throw `Disallowed character on L${start + i + 1}`;
+        throw new Error(`Disallowed character on L${start + i + 1}`);
     });
   });
 }
